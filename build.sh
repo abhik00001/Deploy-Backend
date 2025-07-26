a83c09e05205348c8bc8df5d23178b41b9306182
abhik00001 <sahilk786786123@gmail.com>
2025-07-26 21:30:51 +0530

    create superuser via shell

--- build.sh
+++ build.sh
@@ -7,5 +7,27 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [ "$CREATE_SUPERUSER" = "true" ]; then
-    python manage.py createsuperuser --no-input
+    echo "✅ Creating superuser from env vars..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password and email:
+    user, created = User.objects.get_or_create(username=username)
+    user.email = email
+    user.is_superuser = True
+    user.is_staff = True
+    user.set_password(password)
+    user.save()
+    print("✅ Superuser created or updated.")
+else:
+    print("❌ Missing superuser environment variables.")
+EOF
+else
+    echo "🔕 CREATE_SUPERUSER is not enabled. Skipping superuser creation."
 fi
